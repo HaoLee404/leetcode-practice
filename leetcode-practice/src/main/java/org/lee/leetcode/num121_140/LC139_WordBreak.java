594c3e59bfd1d7e4a3c725b0fb4c25f2bc75c561
lihao <lihao@hst.com>
2023-11-24 19:01:15 +0800

    练习
    Signed-off-by: lihao <lihao@hst.com>

---
+++ leetcode-practice/src/main/java/org/lee/leetcode/num121_140/LC139_WordBreak.java
@@ -0,0 +1,23 @@
+package org.lee.leetcode.num121_140;
+
+import java.util.HashSet;
+import java.util.List;
+
+public class LC139_WordBreak {
+
+    public boolean wordBreak(String s, List<String> wordDict) {
+        boolean[] dp = new boolean[s.length() + 1];
+        dp[0] = true;
+        HashSet<String> set = new HashSet<>(wordDict);
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = 0; j < i; j++) {
+                if (dp[j] && set.contains(s.substring(j, i))) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[s.length()];
+    }
+
+}
